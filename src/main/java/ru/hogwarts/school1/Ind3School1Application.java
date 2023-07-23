39de073dc2feedba535f51c2b250b2611a624f8d
hp <747685@mail.ru>
2023-07-23 18:31:24 +0300

    гарри поппер

---
+++ src/main/java/ru/hogwarts/school1/Ind3School1Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ind3School1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ind3School1Application.class, args);
+    }
+
+    }
---
+++ src/test/java/ru/hogwarts/school1/Ind3School1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ind3School1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
